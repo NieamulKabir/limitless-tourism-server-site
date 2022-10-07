422612fdc24b728eac9b308606476763781622ed
NieamulKabir <kabir000079@gmail.com>
2022-10-08 02:55:57 +0600

    update

--- index.js
+++ index.js
@@ -18,19 +18,79 @@ async function run() {
     try {
         await client.connect();
         const packagesCollection = client.db('limitlessTourism').collection('package');
+        const bookingCollection = client.db('limitlessTourism').collection('bookings');
 
+        //  GET API
         app.get('/package', async (req, res) => {
             const query = {};
+            // RUN FIND OPERATION FOR ALL DATA FROM DATABASE COLLECTION 
             const cursor = packagesCollection.find(query);
+            // CONVERT DATA TO AN ARRAY
             const packages = await cursor.toArray();
             res.send(packages);
         })
-
-        app.get('/package/:id', async(req,res)=>{
+        //  GET SINGLE PACKAGE (READ SPECIFIC DATA FROM SERVER DATABASE)
+        app.get('/package/:id', async (req, res) => {
             const id = req.params.id;
-            const query ={_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const package = await packagesCollection.findOne(query);
             res.send(package);
+        })
+
+        //GET BOOKING information
+        app.get('/bookings', async (req, res) => {
+            const query = {};
+            const cursor = bookingCollection.find(query);
+            const bookings = await cursor.toArray();
+            res.send(bookings)
+        })
+
+        //  GET SINGLE BOOKING (READ SPECIFIC DATA FROM SERVER DATABASE)
+        app.get('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const package = await bookingCollection.findOne(query);
+            res.send(package);
+        })
+
+
+        //POST
+        //Add Single package
+        app.post('/package', async (req, res) => {
+            const newPackage = req.body;
+            const result = await packagesCollection.insertOne(newPackage);
+            res.send(result);
+        })
+
+        //add booking
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result);
+        })
+
+
+        //DELETE
+        app.delete('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await bookingCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        //PUT
+        app.put('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const updateStatus = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    status: updateStatus.status,
+                },
+            };
+            const result = await bookingCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
         })
     }
     finally {
